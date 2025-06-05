11f0e213b6f11a34551bdcac5bb958ab2eb5a313
[Holzgethan] <[hol240981@spengergasse.at]>
2025-06-05 20:52:32 +0200

    commit

--- 2025-06-05/FizzBizz.java
+++ 2025-06-05/FizzBizz.java
@@ -3,7 +3,10 @@ public class FizzBizz{
         var zahl = 0;
         for(int i = 0; i < 100; i++){
             zahl += 1;
-            if(zahl % 3 == 0 ){
+            if (zahl % 3 == 0 && zahl % 5 == 0) {  
+                System.out.println("FizzBizz");
+            } 
+            if(zahl % 3 == 0){
                 System.out.println("Fizz");
             }
             else if(zahl % 5 == 0){
