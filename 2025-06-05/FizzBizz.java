caf291c7f15011c8d7f09605fd26e7bad11d0d5d
[Holzgethan] <[hol240981@spengergasse.at]>
2025-06-05 20:34:27 +0200

    commit

--- 2025-06-05/FizzBizz.java
+++ 2025-06-05/FizzBizz.java
@@ -1,15 +1,17 @@
-public static void ausgabe(){
-    var zahl = 0;
-    for(int i = 100; i > 0; i--){
-        zahl += 1;
-        if(zahl % 3 == 0 ){
-            System.out.println("Fizz");
+public class FizzBizz{
+    public static void ausgabe(){
+        var zahl = 0;
+        for(int i = 0; i < 100; i++){
+            zahl += 1;
+            if(zahl % 3 == 0 ){
+                System.out.println("Fizz");
+            }
+            else if(zahl % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(zahl);
+            }
         }
-        if(zahl % 5 == 0){
-            System.out.println("Buzz");
-        }
-        else{
-            System.out.println(zahl + "\n");
-        }
-    }
+    } 
 }
